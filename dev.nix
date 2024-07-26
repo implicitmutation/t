c66d032d37b710dd56ed06689271ad7f7b8c2468
Harold Shen <hlshen@google.com>
2024-07-26 15:09:29 -0400

    update vsix

--- dev.nix
+++ dev.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }: 
   let firebase-ext = pkgs.fetchurl {
     url =
-      "https://firebasestorage.googleapis.com/v0/b/firemat-preview-drop/o/vsix%2Ffirebase-vscode-0.4.4.vsix?alt=media&token=cae2dc68-f0c8-4ab5-9bbf-2cf6d8f98653";
-    hash = "sha256-QNnztX40gGGkXivvPk+E4XXwSyYMXWFcWJBfuCtE5L8=";
+      "https://firebasestorage.googleapis.com/v0/b/firemat-preview-drop/o/vsix%2Ffirebase-vscode-0.5.2.vsix?alt=media&token=5e3dec76-eac4-400b-9d7e-b79acec59e71";
+    hash = "sha256-dyMPdYyygnx5K+k7Uey2F0xdEfm+5dLyOa+k1dSnAiU=";
     name = "firebase.vsix";
   };
   in {
